54958525f45858058f99520bdf09897c01a6e324
bradh43 <brad.hodkinson2@gmail.com>
2021-02-15 14:44:09 -0500

    update to terms for ownership of feedback

--- src/pages/About/Terms.js
+++ src/pages/About/Terms.js
@@ -56,10 +56,11 @@ export const Terms = (props) => {
         <div className={classes.section}>
           <h1>Terms and Conditions</h1>
           <h1>General Site Usage</h1>
-          <p>Last Revised: February 2, 2021</p>
+          <p>Last Revised: February 15, 2021</p>
           <p>Welcome to curro.us This site is provided as a service to our visitors and may be used for informational purposes only. Because the Terms and Conditions contain legal obligations, please read them carefully.</p>
           <h2>1. YOUR AGREEMENT</h2>
           <p>By using this Site, you agree to be bound by, and to comply with, these Terms and Conditions. If you do not agree to these Terms and Conditions, please do not use this site.</p>
+          <p>You accept that you can not take, modify, edit, distribute, etc. anything from the website, and any suggestions or feedback given is property of Curro and its developers.</p>
           <blockquote>PLEASE NOTE: We reserve the right, at our sole discretion, to change, modify or otherwise alter these Terms and Conditions at any time. Unless otherwise indicated, amendments will become effective immediately. Please review these Terms and Conditions periodically. Your continued use of the Site following the posting of changes and/or modifications will constitute your acceptance of the revised Terms and Conditions and the reasonableness of these standards for notice of changes. For your information, this page was last updated as of the date at the top of these terms and conditions.</blockquote>
           <h2>2. PRIVACY</h2>
           <p>Please review our Privacy Policy, which also governs your visit to this Site, to understand our practices.</p>
